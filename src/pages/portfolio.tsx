d280f36ba1755c9040c5f02b601d69eee96e0a3b
Daniel P. Shannon <daniel@try.works>
2023-09-03 12:28:05 -0400

    🤡 Mock out Impress-style transitions.

---
+++ src/pages/portfolio.tsx
@@ -0,0 +1,26 @@
+import Link from 'next/link'
+import styled from 'styled-components'
+
+const Page = styled.section`
+    display: flex;
+`
+
+export default function Portfolio() {
+    return (
+        <Page>
+            <h1>Portfolio</h1>
+            <ul>
+                <li>
+                    <Link href='/portfolio/paywall'>
+                        Paywall
+                    </Link>
+                </li>
+                <li>
+                    <Link href='/portfolio/maps'>
+                        Maps
+                    </Link>
+                </li>
+            </ul>
+        </Page>
+    )
+}
