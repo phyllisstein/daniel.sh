f8a9dda90cd9dfb287debc7dc2ca04f7997597fc
Daniel P. Shannon <daniel@try.works>
2024-05-02 04:43:38 -0400

    🔧 Drop unnecessary Webpack resolve config,

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
-const path = require('path')
-
-module.exports = {
+export default {
   compiler: {
     styledComponents: true,
   },
@@ -10,21 +8,5 @@ module.exports = {
   reactStrictMode: true,
   typescript: {
     ignoreBuildErrors: true,
-  },
-  webpack(config, { dev }) {
-    config.resolve.enforceExtension = false
-    config.resolve.modules = [
-      path.resolve(__dirname, 'src'),
-      path.resolve(__dirname, 'vendor'),
-      'node_modules',
-      ...config.resolve.modules,
-    ]
-
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: [{ loader: '@svgr/webpack', options: { ref: true, svgo: true } }],
-    })
-
-    return config
   },
 }
