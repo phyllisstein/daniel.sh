21fd054c1c218eb8c1494232489811edbb37ae1e
Daniel P. Shannon <daniel@try.works>
2024-11-14 15:02:35 -0500

    ⚰️ Remove greeking copy.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,10 @@ import { faPaperPlane } from '@fortawesome/sharp-solid-svg-icons'
 
 import {
   ButtonBar,
-  Graf,
   Main,
   Name,
   Section,
-  SectionTitle,
   Tagline,
-  TextContainer,
 } from './page-styles'
 
 export default function Home() {
@@ -37,31 +34,6 @@ export default function Home() {
             </a>
           </div>
         </ButtonBar>
-      </Section>
-      <Section>
-        <SectionTitle>Hi there.</SectionTitle>
-        <TextContainer>
-          <Graf>
-            And guess what else is back. My breakfast? My friskiness. Mama horny
-            Michael. I am having a love affair with this ice cream sandwich. Everyone’s
-            laughing, and riding, and cornholing except Buster. Oh…yeah…the guy in the…the
-            $4,000 suit is holding the elevator for a guy who doesn’t make that in three
-            months. Come on!
-          </Graf>
-          <Graf>
-            Do you guys know where I could get one of those gold necklaces with the T on it?
-            That’s a cross. Across from where?
-          </Graf>
-          <Graf>
-            And the soup of the day is bread. Heyyyy uncle father Oscar. The only thing I
-            found in the fridge was a dead dove in a bag. She keeps saying that God is going
-            to show me a sign. The… something of my ways. Wisdom?
-          </Graf>
-          <Graf>
-            Michael, look, this has got to stop. I mean, flattered? Yes. Interested? Not
-            tonight. Interfere? I ought to pull down your pants and spank your ass raw.
-          </Graf>
-        </TextContainer>
       </Section>
     </Main>
   )
