2cd46a5c436c0b42d75d1b279ca1d489f91f3a40
Daniel P. Shannon <daniel@try.works>
2024-12-27 17:47:09 -0500

    💩 Algorithm doesn't work.

--- src/styles/theme/palette-coloring.ts
+++ src/styles/theme/palette-coloring.ts
@@ -1,16 +1,61 @@
-import { formatCss, inGamut, converter, parse, clampGamut, toGamut } from "culori";
-import * as R from "ramda";
-import _ from "lodash";
+import { formatCss, inGamut, converter, parse } from "culori";
 
-export const toLCH = converter("oklch");
-export const toP3 = toGamut("p3", "oklch");
-const round = _.partial(_.round, _.partial.placeholder, 3);
-export const maxOutP3 = R.pipe(
-    parse,
-    toLCH,
-    R.assoc("c", 1500),
-    toP3,
-    R.map(round),
-    R.assoc("mode", "p3"),
-    formatCss,
-);
+const toLCH = converter("oklch");
+const toP3 = converter("p3");
+const inP3Gamut = inGamut("p3");
+
+function findMaxChroma(l: number, h: number): number {
+    let lower = 0;
+    let upper = 150;
+    let maxChroma = 0;
+
+    // Perform a binary search to find the maximum chroma
+    for (let i = 0; i < 20; i++) {
+        const mid = (lower + upper) / 2;
+        const color = { c: mid, h, l, mode: "oklch" };
+        if (inP3Gamut(color)) {
+            maxChroma = mid;
+            lower = mid;
+        } else {
+            upper = mid;
+        }
+    }
+
+    return maxChroma;
+}
+
+export function rgbToP3(rgb: string): string {
+    const color = parse(rgb);
+    const lchColor = toLCH(color);
+    const originalP3 = formatCss(toP3(color), { format: "p3" });
+
+    if (
+        !lchColor
+        || typeof lchColor.l !== "number"
+        || typeof lchColor.h !== "number"
+    ) {
+        return rgb;
+    }
+
+    let { c: originalC, h, l } = lchColor;
+    const maxChroma = findMaxChroma(lchColor.l, lchColor.h);
+
+    if (originalC > maxChroma) {
+        const maxChromaColor = { c: maxChroma, h, l, mode: "oklch" };
+        const p3Color = toP3(maxChromaColor);
+        const cssColor = formatCss(p3Color, { format: "p3" });
+        // return [originalP3, cssColor, cssColor, cssColor]
+        return originalP3;
+    }
+
+    const steps = 3;
+    const chromaValues = [];
+    for (let i = 0; i < steps; i++) {
+        const newC = originalC + ((maxChroma - originalC) * i) / steps;
+        const color = { c: newC, h, l, mode: "oklch" };
+        const p3Color = toP3(color);
+        const cssColor = formatCss(p3Color, { format: "p3" });
+        chromaValues.push(cssColor);
+    }
+    return originalP3;
+}
