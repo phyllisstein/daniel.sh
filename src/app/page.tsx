fe6939206ab7fdf551e0cfe742f635c01533b9a6
Daniel P. Shannon <daniel@try.works>
2026-03-01 04:35:22 -0500

    🍱 Use SF Symbols icons.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,3 @@
-import { faLinkedinIn, faTwitter } from "@fortawesome/free-brands-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPaperPlane } from "@fortawesome/sharp-solid-svg-icons";
-
 import {
     ButtonBar,
     Graf,
@@ -12,6 +8,11 @@ import {
     Tagline,
     TextContainer,
 } from "./page-styles";
+import PaperPlane from "assets/icons/paper-plane.svg";
+import Bluesky from "assets/icons/bluesky.svg";
+import LinkedIn from "assets/icons/linkedin.svg";
+import { ActionButtonIcon } from "components/icon";
+
 
 export default function Home() {
     return (
@@ -25,19 +26,59 @@ export default function Home() {
                 <Name>
                     You can call me Daniel.
                 </Name>
-                <ButtonBar className="spectrum-QuickActions-overlay">
-                    <div className="spectrum-ActionGroup spectrum-ActionGroup--sizeM">
-                        <a className="spectrum-ActionButton spectrum-ActionButton--sizeXL spectrum-ActionButton--quiet spectrum-ActionGroup-item" href="mailto:daniel@daniel.sh">
-                            <FontAwesomeIcon fixedWidth className="spectrum-Icon spectrum-Icon--sizeM spectrum-ActionButton-icon" icon={ faPaperPlane } />
+                <div className=" spectrum-ActionGroup spectrum-ActionGroup--sizeM ">
+
+
+                    <button aria-controls="" aria-pressed="false" className="spectrum-ActionButton spectrum-ActionButton--sizeM spectrum-ActionGroup-item " data-testid="actionbutton-yd682" id="actionbutton-yd682" role="button">
+
+                        <svg aria-hidden="true" aria-labelledby="Edit" className=" spectrum-Icon spectrum-Icon--sizeM spectrum-ActionButton-icon " focusable="false" id="icon-5ebph" role="img">
+                            <title id="Edit">Edit</title>
+                            <use href="#spectrum-icon-18-Edit" xlinkHref="#spectrum-icon-18-Edit"></use>
+                        </svg>
+                        <span className="spectrum-ActionButton-label">Edit</span>
+                    </button>
+
+                    <button aria-controls="" aria-pressed="false" className=" spectrum-ActionButton spectrum-ActionButton--sizeM spectrum-ActionGroup-item " data-testid="actionbutton-pwrzc" id="actionbutton-pwrzc" role="button">
+
+                        <svg aria-hidden="true" aria-labelledby="Copy" className=" spectrum-Icon spectrum-Icon--sizeM spectrum-ActionButton-icon " focusable="false" id="icon-vz8ho" role="img">
+                            <title id="Copy">Copy</title>
+                            <use href="#spectrum-icon-18-Copy" xlinkHref="#spectrum-icon-18-Copy"></use>
+                        </svg>
+                        <span className="spectrum-ActionButton-label">Copy</span>
+                    </button>
+
+                    <button aria-controls="" aria-pressed="true" className=" spectrum-ActionButton spectrum-ActionButton--sizeM is-selected spectrum-ActionGroup-item " data-testid="actionbutton-5b5i4" id="actionbutton-5b5i4" role="button">
+
+                        <svg aria-hidden="true" aria-labelledby="Delete" className=" spectrum-Icon spectrum-Icon--sizeM spectrum-ActionButton-icon " focusable="false" id="icon-sw0p8" role="img">
+                            <title id="Delete">Delete</title>
+                            <use href="#spectrum-icon-18-Delete" xlinkHref="#spectrum-icon-18-Delete"></use>
+                        </svg>
+                        <span className="spectrum-ActionButton-label">Delete</span>
+                    </button>
+
+                </div>
+                <ButtonBar>
+                    <div className="spectrum-ActionGroup spectrum-ActionGroup--sizeM spectrum-ActionGroup--compact">
+                        <a className="spectrum-ActionButton spectrum-ActionButton--sizeM spectrum-ActionGroup-item" href="mailto:daniel@daniel.sh">
+                            <ActionButtonIcon label="Email me">
+                                <PaperPlane />
+                            </ActionButtonIcon>
                         </a>
-                        <a className="spectrum-ActionButton spectrum-ActionButton--sizeXL spectrum-ActionButton--quiet spectrum-ActionGroup-item" href="https://twitter.com/phyllisstein">
-                            <FontAwesomeIcon fixedWidth className="spectrum-Icon spectrum-Icon--sizeM spectrum-ActionButton-icon" icon={ faTwitter } />
+                        <a className="spectrum-ActionButton spectrum-ActionButton--sizeM spectrum-ActionGroup-item" href="https://twitter.com/phyllisstein">
+                            <ActionButtonIcon label="Connect with me on LinkedIn">
+                                <LinkedIn />
+                            </ActionButtonIcon>
                         </a>
-                        <a className="spectrum-ActionButton spectrum-ActionButton--sizeXL spectrum-ActionButton--quiet spectrum-ActionGroup-item" href="https://linkedin.com/in/danielsh1">
-                            <FontAwesomeIcon fixedWidth className="spectrum-Icon spectrum-Icon--sizeM spectrum-ActionButton-icon" icon={ faLinkedinIn } />
+                        <a className="spectrum-ActionButton spectrum-ActionButton--sizeM spectrum-ActionGroup-item" href="https://linkedin.com/in/danielsh1">
+                            <ActionButtonIcon label="Follow me on BLuesky">
+                                <Bluesky />
+                            </ActionButtonIcon>
                         </a>
                     </div>
                 </ButtonBar>
+            </Section>
+            <Section>
+                <SectionTitle>About Me</SectionTitle>
             </Section>
         </Main>
     );
