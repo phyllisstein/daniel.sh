b07b8f1ee139805941ad150bff48fc85afe1aa6b
Daniel P. Shannon <daniel@try.works>
2026-08-20 02:19:08 -0400

    Fix vinext issues

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,6 @@ export default function Layout ({ children }: { children: ReactNode }) {
         <html lang="en-us">
             <body>
                 <StyledComponentsRegistry>
-                    <Preflight />
                     <Body />
                     <Charlie />
                     <MaisonNeue />
